214856c0e60c54f6866f25f2be8de79b3be6c16d
Bharath0314 <bharathreddy0796@gmail.com>
2024-10-13 12:18:59 +0530

    Add files via upload

---
+++ java/com/tapfoods/controller/RestaurantServlet.java
@@ -0,0 +1,25 @@
+package com.tapfoods.controller;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.tapfoods.daoimpl.RestaurantDaoImpl;
+import com.tapfoods.model.Restaurant;
+@WebServlet("/CallingRestaurantServlet")
+public class RestaurantServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RestaurantDaoImpl restaurant=new RestaurantDaoImpl();
+		List<Restaurant>restaurants=restaurant.getAll();
+		HttpSession ses=req.getSession();
+		ses.setAttribute("restaurantList", restaurants);
+		RequestDispatcher rd=req.getRequestDispatcher("Restaurant.jsp");
+		rd.forward(req, resp);
+	}
+}
---
+++ java/com/tapfoods/dao/OrderItemDao.java
@@ -0,0 +1,17 @@
+package com.tapfoods.dao;
+
+import java.util.List;
+
+import com.tapfoods.model.OrderItem;
+
+public interface OrderItemDao {
+
+	
+	void addOrderItem(OrderItem orderItem);
+	OrderItem getOrderItem(int orderItemId);
+	void UpdateOrderItem(OrderItem orderItem);
+	List<OrderItem> getAllOrderItemsByUserId(int userid);
+	void deleteOrderItem(int orderItemId);
+	List<OrderItem>getAll();
+
+}
